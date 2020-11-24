82c7c06ced8474003488c6c7511f37cd85aaec54
Innocent Zenda <zendainnocent@gmail.com>
2020-11-24 14:53:11 +0300

    Initial commit

---
+++ README.rst
@@ -0,0 +1,3 @@
+Create Django Project
+=====================
+The command line interface for creating Django project.
---
+++ tests/test_create_django_project.py
@@ -0,0 +1,5 @@
+from create_django_project import __version__
+
+
+def test_version():
+    assert __version__ == '0.0.1'
